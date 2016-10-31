e04d58f0de32ab162db3baaf128dd89839e8d249
zyssky <2462084760@qq.com>
2016-10-31 18:52:10 +0800

    修改了登录窗口，添加注册窗口

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.administrator.myapplication;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,12 +17,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.os.StrictMode;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -32,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     List<Map<String,Object>> mapList;
     Button btn_add;
     int choose_item_position;
+    AlertDialog dialog_login;
+    AlertDialog dialog_register;
+    ProgressDialog dialog_progress;
 
     public final String TAG = MainActivity.class.getSimpleName();
 
@@ -46,46 +54,42 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater inflater = getLayoutInflater();
         final View v = inflater.inflate(R.layout.login_dialog, null);
         //LET render login page first
-        new AlertDialog.Builder(this).setTitle("用户登录").setView(v).setPositiveButton(
-                "登录", new DialogInterface.OnClickListener() {
-                    //Because Cannot Acquire View handler directly
-                    //So ReInit a view and set it points to target view.
-                    //Duplicate here!
-
-                    EditText Username = (EditText)v.findViewById(R.id.Username);
-                    EditText Password = (EditText)v.findViewById(R.id.Password);
+        final EditText Username = (EditText)v.findViewById(R.id.Username);
+        final EditText Password = (EditText)v.findViewById(R.id.Password);
 
 
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //deal with the login event
+
+        dialog_login = new AlertDialog.Builder(this).setTitle("用户登录").setView(v).setPositiveButton(
+                "登录",null).setNegativeButton("注册",null).setCancelable(false).create();
 
 
-                                Connector conn = new Connector(Username.getText().toString(), Password.getText().toString());
-                            if(conn.Check_login()){
-                                Log.d("Login-Msg", "LoginSucceed");
-                                    Toast.makeText(getApplicationContext(),"Login Succeed.",Toast.LENGTH_SHORT).show();
-                                }else {
-                            //Bounce a dialog and exit app.
-                                Log.d("Login-Msg", "LoginFailed");
-                                Toast.makeText(getApplicationContext(),"Login Failed.",Toast.LENGTH_SHORT).show();
-                                try{
-                                Thread.sleep(1000);
-                                }
-                                catch(Exception e){e.printStackTrace();}
-                                finally{
-                                    System.exit(0);
-                                }
-                        }
-                    }
-                }
+        dialog_login.show();
 
-        ).setNegativeButton("注册", new DialogInterface.OnClickListener() {
+        dialog_login.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //deal with the register event
+            public void onClick(View v) {
+                dialog_login.hide();
+                showProgressDialog("正在登陆。。。");
+                Connector conn = new Connector(Username.getText().toString(), Password.getText().toString());
+                if(conn.Check_login()){
+                    Log.d("Login-Msg", "LoginSucceed");
+                    Toast.makeText(getApplicationContext(),"Login Succeed.",Toast.LENGTH_SHORT).show();
+                    dialog_login.dismiss();
+                }
+                else{
+                    Log.d("Login-Msg", "LoginFailed");
+                    Toast.makeText(getApplicationContext(),"Login Failed.",Toast.LENGTH_SHORT).show();
+                }
             }
-        }).setCancelable(false).show();
+        });
+
+        dialog_login.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog_login.hide();
+                showRegisterDialog();
+            }
+        });
 
 
         //INIT init thread
@@ -134,6 +138,55 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,0);
             }
         });
+    }
+
+    void showProgressDialog(String message){
+        dialog_progress = new ProgressDialog(this);
+        dialog_progress.setMessage(message);
+        dialog_progress.setCancelable(false);
+        dialog_progress.show();
+    }
+
+    void dismissProgressDialog(){
+        dialog_progress.dismiss();
+    }
+
+
+    void showRegisterDialog(){
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View v = inflater.inflate(R.layout.register_dialog, null);
+        //LET render login page first
+        final EditText Username = (EditText)v.findViewById(R.id.Username_4_register);
+        final EditText Password = (EditText)v.findViewById(R.id.Password_4_register);
+        final EditText Password2 = (EditText)v.findViewById(R.id.Password2_4_register);
+
+        dialog_register = new AlertDialog.Builder(this).setTitle("用户注册").setView(v).setPositiveButton(
+                "确认",null).setNegativeButton("取消",null).setCancelable(false).create();
+
+        dialog_register.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+
+                dialog_register.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog_register.dismiss();
+
+                    }
+                });
+
+                dialog_register.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog_register.dismiss();
+                        dialog_login.show();
+                    }
+                });
+            }
+        });
+
+        dialog_register.show();
     }
 
     @Override
